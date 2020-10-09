813c9f4ba75a05cb4eb325efc4f3353656519bc4
Hud-Volodymyr <v.gud2001@gmail.com>
2020-10-09 15:20:18 +0300

    Fix query parameters problem, add upload formats check, film validation

--- cli-tool/requests.js
+++ cli-tool/requests.js
@@ -1,7 +1,17 @@
+/* eslint-disable require-jsdoc */
 'use strict';
 
 const http = require('http');
 const fs = require('fs');
+const {URL} = require('url');
+
+const formats = ['DVD', 'VHS', 'Blu-Ray'];
+const validYear = (year) => (+year >=1850 && +year<=2020) ? 1 : 0;
+const validFormat = (format) => (formats.includes(format) ? 1 : 0);
+function hasDuplicates(array) {
+  return (new Set(array)).size !== array.length;
+}
+
 const commands = {
   help() {
     console.log('Commands:', Object.keys(commands).join(', '));
@@ -18,7 +28,9 @@ const commands = {
     });
   },
   getFilm(filmname) {
-    http.get(`http://localhost:3000/films/${filmname}`, (resp) => {
+    const searchUrl = new URL('http://localhost:3000/films/filmName');
+    searchUrl.searchParams.set('filmName', filmname);
+    http.get(searchUrl, (resp) => {
       let data = '';
       resp.on('data', (chunk) => {
         data += chunk;
@@ -29,7 +41,9 @@ const commands = {
     });
   },
   getFilmByActor(actorname) {
-    http.get(`http://localhost:3000/actors/${actorname}`, (resp) => {
+    const searchUrl = new URL('http://localhost:3000/actors/actorName');
+    searchUrl.searchParams.set('actorName', actorname);
+    http.get(searchUrl, (resp) => {
       let data = '';
       resp.on('data', (chunk) => {
         data += chunk;
@@ -40,6 +54,18 @@ const commands = {
     });
   },
   addFilm(filmName, releaseYear, formatName, actors) {
+    if (!validYear(releaseYear)) {
+      console.log('Year should be between 1850 and 2020!');
+      return;
+    }
+    if (!validFormat(formatName)) {
+      console.log(`Format should be one of 3: ${formats.join(', ')}`);
+      return;
+    }
+    if (hasDuplicates(actors.split(', '))) {
+      console.log('Actors should have unique names!');
+      return;
+    }
     const data = JSON.stringify({
       name: filmName,
       year: releaseYear,
@@ -53,16 +79,22 @@ const commands = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Content-Length': data.length,
+        'Content-Length': Buffer.byteLength(data),
       },
     };
     const req = http.request(options, (res) => {
       res.on('data', (data) => {
         const jsoned = JSON.parse(data);
-        delete jsoned.id1;
-        jsoned.actors = jsoned.actors.join(', ');
-        console.table(jsoned);
-        console.log('Press enter to proceed');
+        if (jsoned.id1) {
+          delete jsoned.id1;
+          jsoned.actors = jsoned.actors.join(', ');
+          console.table(jsoned);
+          console.log('Press enter to proceed');
+        } else {
+          const jsoned = JSON.parse(data);
+          console.log(jsoned.message);
+          console.log('Press enter to proceed');
+        }
       });
     });
 
@@ -84,7 +116,6 @@ const commands = {
       },
     };
     const req = http.request(options, (res) => {
-      console.log(`statusCode: ${res.statusCode}`);
       res.on('data', (data) => {
         const jsoned = JSON.parse(data);
         console.log(jsoned.message);
@@ -98,15 +129,35 @@ const commands = {
     req.end();
   },
   uploadFile(filepath) {
-    const options = {
-      hostname: 'localhost',
-      port: 3000,
-      path: '/films/upload',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/csv',
-      },
-    };
+    let options = {};
+    const filename = filepath.split('/').pop();
+    const extention = filename
+        .slice((Math.max(0, filename.lastIndexOf('.')) || Infinity) + 1);
+    console.log(extention);
+    if (extention === 'csv') {
+      options = {
+        hostname: 'localhost',
+        port: 3000,
+        path: '/films/upload',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/csv',
+        },
+      };
+    } else if (extention === 'txt') {
+      options = {
+        hostname: 'localhost',
+        port: 3000,
+        path: '/films/upload',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+      };
+    } else {
+      console.log('Only txt and csv file extensions are allowed!');
+      return;
+    }
     const req = http.request(options, (res) => {
       res.on('data', (data) => {
         const jsoned = JSON.parse(data);
