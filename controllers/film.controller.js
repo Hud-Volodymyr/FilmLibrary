bd06ff5b285ed074f82bf9897d32d0a8200f9ce7
Hud-Volodymyr <v.gud2001@gmail.com>
2020-10-13 18:28:08 +0300

    Server should return only json now

--- controllers/film.controller.js
+++ controllers/film.controller.js
@@ -1,9 +1,10 @@
 'use strict';
 const Film = require('../models/film.model.js');
+
 module.exports = {
   addFilm: (req, res) => {
     if (!req.body) {
-      res.status(400).send({message: 'Film can\'t be empty!'});
+      res.status(400).json({message: 'Film can\'t be empty!'});
     }
     if (req.body.name && req.body.year && req.body.format && req.body.actors) {
       const film = new Film({
@@ -14,21 +15,22 @@ module.exports = {
       // actors is a string so we split it into array
       });
       Film.add(film, (err, data) => {
+        if (res.writableEnded) return;
         if (err) {
           if (err == 409) {
-            res.status(409).send({
+            res.status(409).json({
               message: 'This film already exists',
             });
           } else {
-            res.status(500).send({
+            res.status(500).json({
               message: err.message||
           'Unknown error occurred while adding a new film',
             });
           }
-        } else res.send(data);
+        } else res.json(data);
       });
     } else {
-      res.status(422).send('Invalid Input');
+      res.status(422).json('Invalid Input');
     }
   },
   deleteFilm: (req, res) => {
@@ -36,28 +38,28 @@ module.exports = {
     Film.delete(req.params.filmName, (err, data) => {
       if (err) {
         if (err.kind === 'not_found') {
-          res.status(404).send({
+          res.status(404).json({
             message:
           `Didn't find film called ${req.params.filmName.replace(/_/g, ' ')}.`,
           });
         } else {
-          res.status(500).send({
+          res.status(500).json({
             message: 'Could not delete film with name ' + req.params.filmName,
           });
         }
       } else {
-        res.send({message: 'Film info was deleted successfully!'});
+        res.json({message: 'Film info was deleted successfully!'});
       }
     });
   },
   showFilms: (req, res) => {
     Film.getAll((err, data) => {
       if (err) {
-        res.status(500).send({
+        res.status(500).json({
           message:
           err.message || 'Some error occurred while retrieving films.',
         });
-      } else res.send(data);
+      } else res.json(data);
     });
   },
   findByName: (req, res) => {
@@ -67,19 +69,19 @@ module.exports = {
         if (err) {
           console.log(err);
           if (err.kind === 'not_found') {
-            res.status(404).send({
+            res.status(404).json({
               message: `Did not find film with name ${filmName}.`,
             });
             res.end();
           } else {
-            res.status(500).send({
+            res.status(500).json({
               message: 'Error retrieving film with name ' +
               filmName,
             });
             res.end();
           }
         } else {
-          res.send(data);
+          res.json(data);
           res.end();
         }
       });
@@ -93,21 +95,21 @@ module.exports = {
         Film.findByActor(elements, (err, data) => {
           if (err) {
             if (err.kind === 'not_found') {
-              res.status(404).send({
+              res.status(404).json({
                 message: `Did not find film with actor ${actorName}.`,
               });
             } else {
-              res.status(500).send({
+              res.status(500).json({
                 message: 'Error retrieving film with actor ' +
               actorName,
               });
             }
           } else {
-            res.send(data);
+            res.json(data);
           };
         });
       } else {
-        res.status(422).send({
+        res.status(422).json({
           message: 'Invalid input: ' + actorName,
         });
       }
@@ -116,20 +118,31 @@ module.exports = {
   uploadFile: (req, res) => {
     const chunks = [];
     let data = '';
+    const contentType = req.headers['content-type'];
     req.on('data', (chunk) => {
       chunks.push(chunk);
     }).on('end', () => {
       data = Buffer.concat(chunks).toString();
-      data = data.split(/\n/g);
-      console.log(data);
-      data = data.map((str) => str.split(','));
+      if (contentType.match('text/csv')) {
+        data = data.split(/\n/g);
+        console.log(data);
+        data = data.map((str) => str.split(','));
+      } else if (contentType.match('text/plain')) {
+        data = data.split(/\n|\n\n/g);
+        data = data.map((val) => val.substring(val.indexOf(':') + 1)
+            .trim())
+            .join('| ')
+            .split('| | ')
+            .filter((item) => item)
+            .map((str) => str.split('| '));
+      }
       Film.multiple(data, (err, data) => {
         if (err) {
-          res.status(500).send({
+          res.status(500).json({
             message: err.message||
           'Unknown error occurred while adding a new film',
           });
-        } else res.send(data);
+        } else res.json(data);
       });
     });
   },
